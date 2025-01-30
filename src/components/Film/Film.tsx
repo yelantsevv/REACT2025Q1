f172add389036107f51a2b6a4505962e4b004d4c
yelantsevv <yelantsevv@gmail.com>
2025-01-30 19:54:28 +0200

    fix: implement correct loading of film list

--- src/components/Film/Film.tsx
+++ src/components/Film/Film.tsx
@@ -4,12 +4,15 @@ import type { Films, FilmType } from '../../types/types';
 import styles from './Film.module.css';
 
 export default class Film extends Component<FilmType> {
+  state: { filmData: Films | undefined } = { filmData: undefined };
+
   async getFilm() {
     if (this.props.state.has(this.props.film)) {
-      return this.props.state.get(this.props.film);
+      this.setState({ filmData: this.props.state.get(this.props.film) });
     } else {
       const rez = await getData<Films>(this.props.film);
       this.props.state.set(this.props.film, rez);
+      this.setState({ filmData: rez });
     }
   }
 
@@ -18,16 +21,14 @@ export default class Film extends Component<FilmType> {
   }
 
   render() {
-    const film = this.props.state.get(this.props.film);
+    const { filmData } = this.state;
     return (
-      <>
-        <li
-          className={film?.title ? '' : styles.loading}
-          title={film?.opening_crawl}
-        >
-          {film?.title}
-        </li>
-      </>
+      <li
+        className={filmData?.title ? '' : styles.loading}
+        title={filmData?.opening_crawl}
+      >
+        {filmData?.title}
+      </li>
     );
   }
 }
