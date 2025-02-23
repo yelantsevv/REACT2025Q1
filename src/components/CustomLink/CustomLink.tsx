f4c0fd39430d26c0569650a2a429c6b0d81fb98c
yelantsevv <yelantsevv@gmail.com>
2025-02-23 16:31:06 +0200

    feat: add spinner

--- src/components/CustomLink/CustomLink.tsx
+++ src/components/CustomLink/CustomLink.tsx
@@ -5,21 +5,34 @@ import type { CustomLinkProps } from '../../types/types';
 import Link from 'next/link';
 import { useLocalStorage } from '../../hooks';
 import { useSearchParams } from 'next/navigation';
+import { useTransition } from 'react';
+import Spinner from '../Spinner/Spinner';
 export default function CustomLink({ query, item }: CustomLinkProps) {
   const page = useSearchParams().get('page');
   const [, setValue] = useLocalStorage('query');
+  const [isPending, startTransition] = useTransition();
+
+  const handlePageChange = () => {
+    startTransition(() => {
+      setValue(query);
+    });
+  };
+
   return (
-    <Link
-      href={query || ''}
-      data-testid="link"
-      className={clsx(
-        styles.button,
-        !query && styles.inactive,
-        (page || '1') == item && styles.active
-      )}
-      onClick={() => setValue(query)}
-    >
-      {item}
-    </Link>
+    <>
+      {isPending && <Spinner />}
+      <Link
+        href={query || ''}
+        data-testid="link"
+        className={clsx(
+          styles.button,
+          !query && styles.inactive,
+          (page || '1') == item && styles.active
+        )}
+        onClick={handlePageChange}
+      >
+        {item}
+      </Link>
+    </>
   );
 }
